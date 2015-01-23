c926759005d8093e9e1441fb8f836918f572499f
Juliana Leclaire <juliana.leclaire@gmail.com>
2015-01-23 11:58:43 +0100

    maj

--- js/reuse.js
+++ js/reuse.js
@@ -721,7 +721,7 @@ function TimeLine(options) {
     		d0 = my.data()[i - 1],
     		d1 = my.data()[i],
     		d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-
+    		    		
     		my.graph().select("circle.y")
     		.style("opacity", 1)
     		.attr("transform",
